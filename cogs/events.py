98c7ac279c2780c601d487df682e84876bed3f39
DanilMirov <32481690+DanilMirov@users.noreply.github.com>
2018-01-28 19:27:04 +0500

    Delete events.py

--- cogs/events.py
+++
@@ -1,1 +0,0 @@
-from discord.ext import commandsclass Events:    def __init__(self, bot):        self.bot = bot    async def on_ready(self):        print('Ready!')def setup(bot):    bot.add_cog(Events(bot))
