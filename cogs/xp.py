98982521e410a3c63763f6e337378b42e75c0182
DanilMirov <32481690+DanilMirov@users.noreply.github.com>
2018-01-29 13:04:24 +0000

    Добавление комманды xp
    Чтобы получить XP, надо написать s.xp, и потом писать сообщения.

---
+++ cogs/xp.py
@@ -0,0 +1,30 @@
+import discord
+from discord.ext import commands
+
+import sqlite3
+data = sqlite3.connect('data.sqlite')
+cur = data.cursor()
+
+class XP:
+    def __init__(self, bot):
+        self.bot = bot
+
+    async def on_message(self, message):
+        if message.author.bot: return
+        cur.execute('SELECT * FROM xp WHERE id="{0}"'.format(message.author.id))
+        try:
+            res = cur.fetchone()[1] + len(message.content)
+            cur.execute('UPDATE xp SET xp={0} WHERE id={1}'.format(res, message.author.id)); data.commit()
+        except: pass
+    @commands.command(pass_context=True, description='XP')
+    async def xp(self, ctx):
+        cur.execute('SELECT * FROM xp WHERE id="{0}"'.format(ctx.message.author.id))
+        try: await self.bot.say('XP: {0}'.format(cur.fetchone()[1]))
+        except:
+            await self.bot.say('У вас нет XP')
+            cur.execute('INSERT INTO xp VALUES ("{0}", 0)'.format(ctx.message.author.id)); data.commit()
+            cur.execute('SELECT * FROM xp WHERE id="{0}"'.format(ctx.message.author.id))
+            await self.bot.say('XP: {0}'.format(cur.fetchone()[1]))
+
+def setup(bot):
+    bot.add_cog(XP(bot))
