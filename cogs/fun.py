b7ead7c3ecf25a3b13525ddd3f9a64d14596ef5f
DanilMirov <32481690+DanilMirov@users.noreply.github.com>
2018-01-29 10:17:30 +0000

    Изменение комманд, добавление комманды birb

--- cogs/fun.py
+++ cogs/fun.py
@@ -1,38 +1,49 @@
 import discord
 from discord.ext import commands
 from urllib.request import Request, urlopen
-import random
+import random, json
 
-def color(ctx):
-    color = '6553855'
-    if ctx.message.server is not None:
-        color = ctx.message.server.me.color
-    return color
+color = 6553855
+headers = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11',
+           'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
+           'Accept_Encoding': 'gzip, deflate',
+           'Accept_Language': 'ru-RU,en-US;q=0.8',
+           'Connection': 'keep-alive',
+           'Referer': 'https://www.google.ru/',
+           'X_Requested_With': 'simple.bot'}
 
 class Fun:
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.command(pass_context=True, description='Рандомный кот')
-    async def cat(self, ctx):
-        cat = urlopen('https://random.cat/meow')
-        cat = str(cat.read())
-        cat = cat[11:]
-        cat = cat[:len(cat) - 3]
-        cat = cat.replace("\\", "")
-        embed = discord.Embed(title="Рандомный кот", color=color(ctx))
-        embed.set_footer(text="{}".format(str(cat)))
-        embed.set_image(url=str(cat))
+    @commands.command(name='cat', description='Рандомный кот')
+    async def _cat(self):
+        url = urlopen('https://random.cat/meow')
+        cat = json.loads(url.read().decode())
+        cat = cat['file']
+        embed = discord.Embed(title="Рандомный кот", color=color)
+        embed.set_footer(text=cat)
+        embed.set_image(url=cat)
         await self.bot.say(embed=embed)
-    @commands.command(pass_context=True, description='Рандомная собака')
-    async def dog(self, ctx):
-        dog = urlopen('https://random.dog/woof')
-        dog = str(dog.read())
-        dog = dog[2:]
-        dog = dog[:len(dog) - 1]
-        embed = discord.Embed(title="Рандомный собакен", color=color(ctx))
-        embed.set_footer(text="{}".format("http://random.dog/" + str(dog)))
-        embed.set_image(url="http://random.dog/" + str(dog))
+    @commands.command(name='dog', description='Рандомная собака')
+    async def _dog(self):
+        dog = "https://random.dog/{0}"
+        dog_api = Request('https://random.dog/woof', headers=headers)
+        idphoto = urlopen(dog_api).read().decode("utf-8")
+        dog = dog.format(idphoto)
+        embed = discord.Embed(title="Рандомный собакен", color=color)
+        embed.set_footer(text=dog)
+        embed.set_image(url=dog)
+        await self.bot.say(embed=embed)
+    @commands.command(name='birb', aliases=['bird'], description='Рандомная птичка :3')
+    async def _birb(self):
+        birb = "https://random.birb.pw/img/{0}"
+        birb_api = Request("http://random.birb.pw/tweet/", headers=headers)
+        idphoto = urlopen(birb_api).read().decode("utf-8")
+        birb = birb.format(idphoto)
+        embed = discord.Embed(title="Рандомная птичка :3", color=color)
+        embed.set_footer(text=birb)
+        embed.set_image(url=birb)
         await self.bot.say(embed=embed)
     @commands.command(description="Рандомное число")
     async def random(self, nb1=None, nb2=None):
