0e8766d6881f9c0057e89bed102411279e3b608a
DanilMirov <32481690+DanilMirov@users.noreply.github.com>
2018-01-30 13:36:07 +0000

    Update fun.py

--- cogs/fun.py
+++ cogs/fun.py
@@ -2,8 +2,9 @@ import discord
 from discord.ext import commands
 from urllib.request import Request, urlopen
 import random, json
+import config
 
-color = 6553855
+color = config.color
 headers = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11',
            'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
            'Accept_Encoding': 'gzip, deflate',
