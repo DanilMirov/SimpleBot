fe067b4c831542eb448e0ff4ee9bdd5edcb8025a
DanilMirov <32481690+DanilMirov@users.noreply.github.com>
2018-01-30 13:35:01 +0000

    Update events.py

--- cogs/events.py
+++ cogs/events.py
@@ -1,10 +1,14 @@
+import discord
 from discord.ext import commands
+import config
+
 class Events:
     def __init__(self, bot):
         self.bot = bot
 
     async def on_ready(self):
         print('Ready!')
+        await self.bot.change_presence(game=discord.Game(name=config.game), status=config.status)
 
 def setup(bot):
     bot.add_cog(Events(bot))
