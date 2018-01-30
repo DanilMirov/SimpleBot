6645885e6035da5db79a3ce532785a0fbd8583af
DanilMirov <32481690+DanilMirov@users.noreply.github.com>
2018-01-30 13:42:47 +0000

    Update info.py

--- cogs/info.py
+++ cogs/info.py
@@ -1,5 +1,8 @@
 import discord
 from discord.ext import commands
+import config
+
+color = config.color
 
 class Info:
     def __init__(self, bot):
@@ -7,8 +10,7 @@ class Info:
 
     @commands.command(description='About bot')
     async def about(self):
-        await self.bot.say('Разработчик: 321268938728144906, Исходники: https://github.com/DanilMirov/SimpleBot/')
-
+        await self.bot.say(embed=discord.Embed(description='Разработчик: 321268938728144906, Исходники: https://github.com/DanilMirov/SimpleBot/', color=color))
     @commands.command(pass_context=True, description='Info', aliases=['serverinfo', 'sinfo'])
     async def server(self, ctx):
         server = ctx.message.server
@@ -27,7 +29,25 @@ class Info:
                 voice += 1
         msg += "Каналов: `{0}` Текстовых | `{1}` Голосовых | **{2}** Всего\n".format(text, voice, str(len(server.channels)))
         msg += "Всего ролей: `{0}`\n".format(str(len(server.roles)))
-        await self.bot.say(msg)
+        embed = discord.Embed(description=msg, color=color)
+        await self.bot.say(embed=embed)
+    @commands.command(pass_context=True, description='Avatar', aliases=['a'])
+    async def avatar(self, ctx, user: discord.User=None):
+        if user is None:
+            try: user = ctx.message.mentions[0]
+            except: user = ctx.message.author
+        avt = user.avatar_url
+        embed = discord.Embed(url=avt, color=color)
+        embed.set_author(name=user.name)
+        embed.set_author(name=user.name, icon_url=avt)
+        embed.set_image(url=avt)
+        await self.bot.say(embed=embed)
+    @commands.command(pass_context=True, description='Роли на сервере')
+    async def roles(self, ctx):
+        msg = 'Сдесь есть такие роли:\n'
+        for role in ctx.message.server.roles:
+            msg += '`{0}` '.format(role.name)
+        await self.bot.say(embed=discord.Embed(description=msg, color=color))
 
 def setup(bot):
     bot.add_cog(Info(bot))
